8c160b55b8d58904ecd327dc0c8aa36c8f84e928
Kishan <payadikishan@gmail.com>
2024-06-09 09:10:04 +0530

    .

--- Backend/backend/backend/settings.py
+++ Backend/backend/backend/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
 CORS_ORIGIN_ALLOW_ALL = True
 CSRF_TRUSTED_ORIGINS = [
     'http://127.0.0.1:3000/',
-    'http://localhost:3000/'
+    'http://localhost:3000/',
 ]
 ROOT_URLCONF = 'backend.urls'
 
